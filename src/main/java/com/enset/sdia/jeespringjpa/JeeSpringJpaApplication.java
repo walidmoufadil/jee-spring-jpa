e474648205187dc5c8669129cdb5b8d72fdd98ee
walidmoufadil <walid.moufadil-etu@etu.univh2c.ma>
2025-03-13 17:07:54 +0000

    initial commit

---
+++ src/main/java/com/enset/sdia/jeespringjpa/JeeSpringJpaApplication.java
@@ -0,0 +1,13 @@
+package com.enset.sdia.jeespringjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeeSpringJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JeeSpringJpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/enset/sdia/jeespringjpa/JeeSpringJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enset.sdia.jeespringjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeeSpringJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
