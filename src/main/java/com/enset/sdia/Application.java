68c53d8cdc1ab718116541433107cc67b45ec852
walidmoufadil <walid.moufadil-etu@etu.univh2c.ma>
2025-03-13 17:23:20 +0000

    add Application class

--- src/main/java/com/enset/sdia/Application.java
+++ src/main/java/com/enset/sdia/Application.java
@@ -1,7 +1,15 @@
 package com.enset.sdia;
 
+import com.enset.sdia.entity.Patient;
+import com.enset.sdia.repository.IPatientRepo;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Date;
+import java.util.List;
 
 
 @SpringBootApplication
@@ -11,5 +19,36 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
+    @Bean
+    CommandLineRunner commandLineRunner(ApplicationContext ctx, IPatientRepo iPatientRepo) {
+        return args -> {
+            iPatientRepo.saveAll(
+                    List.of(
+                            Patient.builder()
+                                    .name("John")
+                                    .score(10)
+                                    .malade(true)
+                                    .dateNaissance(new Date("01/01/1970"))
+                                    .build(),
+                            Patient.builder()
+                                    .name("Maria")
+                                    .score(15)
+                                    .malade(false)
+                                    .dateNaissance(new Date("02/02/1980"))
+                                    .build()
+
+                    )
+            );
+
+            iPatientRepo.findAll().forEach(System.out::println);
+
+            iPatientRepo.findById((long) 1);
+
+            iPatientRepo.findByNameContains("John").forEach(System.out::println);
+
+            iPatientRepo.deleteById((long) 1);
+        };
+
+    }
 }
 
