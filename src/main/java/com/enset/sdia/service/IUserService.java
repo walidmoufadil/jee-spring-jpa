9257f8294f1883c8fa806da8cc9116eb1d712561
walidmoufadil <walid.moufadil-etu@etu.univh2c.ma>
2025-03-14 05:09:33 +0000

    add user service

--- src/main/java/com/enset/sdia/service/IUserService.java
+++ src/main/java/com/enset/sdia/service/IUserService.java
@@ -1,2 +1,17 @@
-package com.enset.sdia.service;public interface IUserService {
+package com.enset.sdia.service;
+
+import com.enset.sdia.entity.Role;
+import com.enset.sdia.entity.User;
+
+import java.util.List;
+
+public interface IUserService {
+    User addUser(User user);
+    Role addRole(Role role);
+    List<User> getAllUsers();
+    User getUser(String id);
+    void deleteUser(String id);
+    void addRoleToUser(String username, String rolename);
+    Role findRoleByRolename(String rolename);
+    User authenticate(String username, String password);
 }
