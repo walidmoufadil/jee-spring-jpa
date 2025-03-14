f0557969880835528ba7f63fe35b591cc56bfb96
walidmoufadil <walid.moufadil-etu@etu.univh2c.ma>
2025-03-14 05:08:42 +0000

    add Role Class

--- src/main/java/com/enset/sdia/entity/Role.java
+++ src/main/java/com/enset/sdia/entity/Role.java
@@ -1,2 +1,21 @@
-package com.enset.sdia.entity;public class Role {
+package com.enset.sdia.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data@NoArgsConstructor@AllArgsConstructor@Builder
+public class Role {
+    @Id@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(unique=true,length = 20)
+    private String rolename;
+    private String description;
+    @ManyToMany
+    @ToString.Exclude
+    private List<User> users;
+
 }
