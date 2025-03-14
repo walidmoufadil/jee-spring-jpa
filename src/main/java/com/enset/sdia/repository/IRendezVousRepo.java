f84ea7946bce72c14bd669d30d248cd21adc0acc
walidmoufadil <walid.moufadil-etu@etu.univh2c.ma>
2025-03-14 01:40:40 +0000

    add Rendez-vous Repository

--- src/main/java/com/enset/sdia/repository/IRendezVousRepo.java
+++ src/main/java/com/enset/sdia/repository/IRendezVousRepo.java
@@ -1,15 +1,8 @@
 package com.enset.sdia.repository;
 
-import com.enset.sdia.entity.Patient;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+import com.enset.sdia.entity.RendezVous;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RestResource;
 
-import java.util.List;
+public interface IRendezVousRepo extends JpaRepository<RendezVous, Long> {
 
-@RestResource
-public interface IPatientRepo extends JpaRepository<Patient, Long> {
-    List<Patient> findByNameContains(String name);
-    Page<Patient> findByMalade(boolean malade, Pageable pageable);
 }
