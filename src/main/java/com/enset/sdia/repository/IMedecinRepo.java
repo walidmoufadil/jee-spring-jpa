e96f730e5c74bc412a2d23bbd49680d571eac6bf
walidmoufadil <walid.moufadil-etu@etu.univh2c.ma>
2025-03-14 01:40:11 +0000

    add Medecin Repository

--- src/main/java/com/enset/sdia/repository/IMedecinRepo.java
+++ src/main/java/com/enset/sdia/repository/IMedecinRepo.java
@@ -1,15 +1,10 @@
 package com.enset.sdia.repository;
 
-import com.enset.sdia.entity.Patient;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+import com.enset.sdia.entity.Medecin;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RestResource;
 
 import java.util.List;
 
-@RestResource
-public interface IPatientRepo extends JpaRepository<Patient, Long> {
-    List<Patient> findByNameContains(String name);
-    Page<Patient> findByMalade(boolean malade, Pageable pageable);
+public interface IMedecinRepo extends JpaRepository<Medecin, Long> {
+    List<Medecin> findByNom(String medecinName);
 }
