1357cebc6858e92da0b206906780491b5bd4f24f
walidmoufadil <walid.moufadil-etu@etu.univh2c.ma>
2025-03-14 05:08:08 +0000

    add User Class

--- src/main/java/com/enset/sdia/entity/User.java
+++ src/main/java/com/enset/sdia/entity/User.java
@@ -1,2 +1,19 @@
-package com.enset.sdia.entity;public class User {
+package com.enset.sdia.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data@NoArgsConstructor@AllArgsConstructor@ToString@Builder
+public class User {
+    @Id
+    private String id;
+    @Column(unique=true,length = 20)
+    private String username;
+    private String password;
+    @ManyToMany(mappedBy = "users",fetch = FetchType.EAGER)
+    private List<Role> roles=new ArrayList<>();
 }
