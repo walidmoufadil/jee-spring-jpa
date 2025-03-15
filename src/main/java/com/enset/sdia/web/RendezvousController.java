815d4777c0ca27154159770de5b52c554a25ba45
walidmoufadil <walid.moufadil-etu@etu.univh2c.ma>
2025-03-15 00:01:38 +0000

    add Rendez-vous Controller

--- src/main/java/com/enset/sdia/web/RendezvousController.java
+++ src/main/java/com/enset/sdia/web/RendezvousController.java
@@ -1,2 +1,28 @@
-package com.enset.sdia.web;public class RendezvousController {
+package com.enset.sdia.web;
+
+import com.enset.sdia.entity.RendezVous;
+import com.enset.sdia.service.IRendezVousService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/rendezvous")
+@AllArgsConstructor
+public class RendezvousController {
+    private IRendezVousService rendezVousService;
+
+    @GetMapping("/all")
+    public List<RendezVous> getAllRendezVous() {
+        return rendezVousService.getAllRendezVous();
+    }
+    @GetMapping("/{id}")
+    public RendezVous getRendezVous(@PathVariable Long id) {
+        return rendezVousService.getRendezVousById(id);
+    }
+    @PostMapping("/add")
+    public void addRendezVous(@RequestBody RendezVous rendezVous) {
+        rendezVousService.saveRendezVoud(rendezVous);
+    }
 }
