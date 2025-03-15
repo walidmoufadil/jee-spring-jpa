1ccdc03b4833f4d21a0ee15adf56dc9571a2c3ab
walidmoufadil <walid.moufadil-etu@etu.univh2c.ma>
2025-03-15 00:02:07 +0000

    add Consultation Controller

--- src/main/java/com/enset/sdia/web/ConsultationController.java
+++ src/main/java/com/enset/sdia/web/ConsultationController.java
@@ -1,2 +1,27 @@
-package com.enset.sdia.web;public class ConsultationController {
+package com.enset.sdia.web;
+
+import com.enset.sdia.entity.Consultation;
+import com.enset.sdia.service.IConsultationService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("consultation")
+@AllArgsConstructor
+public class ConsultationController {
+    private IConsultationService consultationService;
+    @GetMapping("/all")
+    public List<Consultation> getAll() {
+        return consultationService.getAllConsultation();
+    }
+    @GetMapping("/{id}")
+    public Consultation getConsultation(@PathVariable Long id) {
+        return consultationService.getConsultationById(id);
+    }
+    @PostMapping("/add")
+    public void addConsultation(@RequestBody Consultation consultation) {
+        consultationService.saveConsultation(consultation);
+    }
 }
