d8da95869d4d6da433fc97d51970503b34933daa
walidmoufadil <walid.moufadil-etu@etu.univh2c.ma>
2025-03-15 00:02:45 +0000

    add User Controller

--- src/main/java/com/enset/sdia/web/UserController.java
+++ src/main/java/com/enset/sdia/web/UserController.java
@@ -1,2 +1,27 @@
-package com.enset.sdia.web;public class UserController {
+package com.enset.sdia.web;
+
+import com.enset.sdia.entity.User;
+import com.enset.sdia.service.IUserService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/user")
+public class UserController {
+    private IUserService userService;
+    @GetMapping("/all")
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable String id) {
+        return userService.getUser(id);
+    }
+    @PostMapping("/add")
+    public void addUser(@RequestBody User user) {
+        userService.addUser(user);
+    }
 }
