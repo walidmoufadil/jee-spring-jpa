d5479c4f318889cc6f12fb52fd3b8324ec539580
walidmoufadil <walid.moufadil-etu@etu.univh2c.ma>
2025-03-15 00:00:52 +0000

    add Medecin Controller

--- src/main/java/com/enset/sdia/web/MedecinController.java
+++ src/main/java/com/enset/sdia/web/MedecinController.java
@@ -1,2 +1,27 @@
-package com.enset.sdia.web;public class MedecinController {
+package com.enset.sdia.web;
+
+import com.enset.sdia.entity.Medecin;
+import com.enset.sdia.service.IMedecinService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/medecin")
+public class MedecinController {
+    private IMedecinService medecinService;
+    @GetMapping("/all")
+    public List<Medecin> getAllMedecin() {
+        return medecinService.getAllMedecin();
+    }
+    @GetMapping("/id")
+    private Medecin getMedecinById(long id) {
+        return medecinService.getMedecinById(id);
+    }
+    @PostMapping("/add")
+    public Medecin addMedecin(@RequestBody Medecin medecin) {
+        return medecinService.saveMedecin(medecin);
+    }
 }
